d85d33cddd5f233c266cf6100cde56b4498508d7
BenDaBeast22 <bencritoph@gmail.com>
2026-07-28 20:00:53 -0400

    fix displayMode

--- components/LyricLine.tsx
+++ components/LyricLine.tsx
@@ -23,6 +23,7 @@ export default function LyricLine({ tokens, songTitle, displayMode }: LyricLineP
           token={token}
           songTitle={songTitle}
           contextSentence={fullSentence}
+          displayMode={displayMode}
         />
       ))}
     </div>
